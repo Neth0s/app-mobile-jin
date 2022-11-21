4fe7495c1d7e5c9efd398f2b8cc4f7eac8795b58
nethos <geeklego@gmail.com>
2022-11-21 14:58:36 +0100

    initial commit

---
+++ app/src/main/java/com/example/appli_mobile_clemence_pierre/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appli_mobile_clemence_pierre
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
